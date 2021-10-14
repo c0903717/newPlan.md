21a5422243d44c8512d7e07f05f2ef393877090a
separase <73293499+separase@users.noreply.github.com>
2021-10-14 08:06:33 -0500

    Update mediadatabase.py

--- mediadatabase.py
+++ mediadatabase.py
@@ -6,11 +6,11 @@ class DemoPanel(wx.Panel):
  
         self.parent = parent
  
-        VerbalButton = wx.Button(self, label="Verbal Mediums",size=(100,100))
-        VerbalButton.Bind(wx.EVT_BUTTON, self.OnMsgBtn )
+        VerbalButton = wx.Button(self, label="Verbal Mediums",size=(100,30))
+        VerbalButton.Bind(wx.EVT_BUTTON, self.OnMsgBtn)
  
-        VisualButton = wx.Button(self, label="Visual Mediums",size=(100,100))
-        VisualButton.Bind(wx.EVT_BUTTON, self.OnMsgBtn )
+        VisualButton = wx.Button(self, label="Visual Mediums",size=(100,30))
+        VisualButton.Bind(wx.EVT_BUTTON, self.OnMsgBtn)
  
         Sizer = wx.BoxSizer(wx.VERTICAL)
         Sizer.Add(VerbalButton, 0, wx.ALIGN_CENTER|wx.ALL, 25)
@@ -19,7 +19,6 @@ class DemoPanel(wx.Panel):
         self.SetSizerAndFit(Sizer)
  
     def OnMsgBtn(self, event=None):
-        """Bring up a wx.MessageDialog with a useless message."""
         dlg = wx.MessageDialog(self,
                                 message='A completely useless message',
                                 caption='A Message Box',
@@ -32,25 +31,17 @@ class DemoFrame(wx.Frame):
     def __init__(self, *args, **kwargs):
         wx.Frame.__init__(self, *args, **kwargs)
  
-        # Build the menu bar
         MenuBar = wx.MenuBar()
- 
         FileMenu = wx.Menu()
  
-        #item = FileMenu.Append(wx.ID_EXIT, text="Quit")
-        #self.Bind(wx.EVT_MENU, self.OnQuit, item)
- 
+        status=self.CreateStatusBar()
+        FileMenu.Append(wx.NewId(),"Open file...")
         MenuBar.Append(FileMenu, "File")
         self.SetMenuBar(MenuBar)
- 
-        # Add the Widget Panel
+
         self.Panel = DemoPanel(self)
  
         self.Fit()
- 
-    def OnQuit(self, event=None):
-        """Exit application."""
-        self.Close()
  
 if __name__ == '__main__':
     app = wx.App()
